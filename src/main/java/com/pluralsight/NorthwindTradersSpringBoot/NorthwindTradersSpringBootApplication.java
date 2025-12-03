e2760e6a6a95d3d9111a380f24f8f67c3921fade
eriinys <eriinys@duck.com>
2025-12-02 22:33:00 -0600

    added ApplicationContext

--- src/main/java/com/pluralsight/NorthwindTradersSpringBoot/NorthwindTradersSpringBootApplication.java
+++ src/main/java/com/pluralsight/NorthwindTradersSpringBoot/NorthwindTradersSpringBootApplication.java
@@ -2,12 +2,13 @@ package com.pluralsight.NorthwindTradersSpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class NorthwindTradersSpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NorthwindTradersSpringBootApplication.class, args);
+        ApplicationContext context = SpringApplication.run(NorthwindTradersSpringBootApplication.class, args);
 	}
 
 }
