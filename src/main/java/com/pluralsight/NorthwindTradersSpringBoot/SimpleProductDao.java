96e80107325a930058f942866b06a93be8740fc6
eriinys <eriinys@duck.com>
2025-12-03 14:36:34 -0600

    Implemented ProductDao interface, added sample products in constructor, and implemented required methods

---
+++ src/main/java/com/pluralsight/NorthwindTradersSpringBoot/SimpleProductDao.java
@@ -0,0 +1,26 @@
+package com.pluralsight.NorthwindTradersSpringBoot;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class SimpleProductDao implements ProductDao {
+    private List<Product> products = new ArrayList<>();
+
+    public SimpleProductDao() {
+        products.add(new Product(1, "Salmon", "Seafood", 22.00));
+        products.add(new Product(2, "Cheese", "Dairy", 8.00));
+    }
+
+    @Override
+    public void add(Product product) {
+        products.add(product);
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return products;
+    }
+}
