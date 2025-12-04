3f370ca94fe5e8605249ff4ffbabdf8bf285cd20
eriinys <eriinys@duck.com>
2025-12-04 10:23:59 -0600

    Created DatabaseConfiguration class that configures datasource from application.properties

---
+++ src/main/java/com/pluralsight/NorthwindTradersSpringBoot/DatabaseConfiguration.java
@@ -0,0 +1,30 @@
+package com.pluralsight.NorthwindTradersSpringBoot;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfiguration {
+    private BasicDataSource basicDataSource;
+
+    @Autowired
+    public DatabaseConfiguration (@Value("${datasource.url}") String url,
+                                  @Value("${datasource.username}") String username,
+                                  @Value("${datasource.password}") String password)
+    {
+        basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(url);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        return basicDataSource;
+    }
+}
