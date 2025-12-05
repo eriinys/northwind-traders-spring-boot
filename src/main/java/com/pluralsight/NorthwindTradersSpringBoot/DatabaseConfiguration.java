02f4374cfca78280ce8a0140df9e0693b89e93c5
eriinys <eriinys@duck.com>
2025-12-04 18:20:10 -0600

    Moved datasource configuration into custom database.properties file and loaded it via @PropertySource

--- src/main/java/com/pluralsight/NorthwindTradersSpringBoot/DatabaseConfiguration.java
+++ src/main/java/com/pluralsight/NorthwindTradersSpringBoot/DatabaseConfiguration.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:database.properties")
 public class DatabaseConfiguration {
     private BasicDataSource basicDataSource;
 
